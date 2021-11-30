2cbe1840c34da0084ae89dfaa516b83a1a7203e3
l-goede <luise.goede@yahoo.de>
2021-11-30 17:37:39 +0100

    iteration 1-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  let firstContacts = contacts.slice(0, 5);
+  const [celeb, setCeleb] = useState(firstContacts);
+
+  function handleAdd() {
+    let randomElem =
+      firstContacts[Math.floor(Math.random() * firstContacts.length)];
+    setCeleb([randomElem, ...firstContacts]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ironcontacts</h1>
+      <button onClick={handleAdd}>Add</button>
+      {celeb.map((elem, index) => {
+        return (
+          <div key={`${elem.id}${index}`}>
+            <ul>
+              <li>
+                <img src={elem.pictureUrl} alt="" />
+              </li>
+              <li>{elem.name}</li>
+              <li>{elem.popularity}</li>
+              <li>{elem.wonOscar === true && "🏆"}</li>
+              <li>{elem.wonEmmy === true && "🏆"}</li>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
